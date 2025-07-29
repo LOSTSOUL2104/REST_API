7a905400eb4c55f7fb535af4a0380e636b295213
LOSTSOUL2104 <godofwar210405@gmail.com>
2025-07-29 21:04:32 +0530

    co

--- routes/bfhlRoutes.js
+++ routes/bfhlRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { handleBFHL } = require("../controllers/bfhlController");
+const { handleBFHL } = require("../controllers/controller");
 
 router.post("/", handleBFHL); 
 
